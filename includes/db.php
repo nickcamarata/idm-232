b8e620b4aafc3b7ffc66a7dbcd7d556dbb80433f
nickcamarata <32547919+nickcamarata@users.noreply.github.com>
2018-05-10 03:25:16 -0400

    Maiden Voyage

---
+++ includes/db.php
@@ -0,0 +1,16 @@
+<?php
+    $dbhost = "localhost";
+    $dbuser ="root";
+    $dbpass = "root";
+    $dbname="example";
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+    if (mysqli_connect_errno()) {
+        die("Database connection failed: " .
+          mysqli_connect_error() .
+          " (" . mysqli_connect_errno() . ")"
+        );
+    }
+    //Everything above this is standard and should almost always be this way
+
+?>
---
+++ main.js
@@ -0,0 +1,19 @@
+function spin() {
+    var audio = new Audio('assets/spin.mp3');
+    audio.play();
+}
+
+function click() {
+    var audio = new Audio('assets/click.mp3');
+    audio.play();
+}
+
+function trumpet() {
+    var audio = new Audio('assets/trumpet.mp3');
+    audio.play();
+}
+
+function sax() {
+    var audio = new Audio('assets/sax.mp3');
+    audio.play();
+}
